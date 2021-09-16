1365df35d21c550eda9f5479e63b8c63af4ab157
Carl Harris <ceharris@vt.edu>
2021-09-16 17:05:37 -0400

    format test case

--- tests/jwt-cli.rs
+++ tests/jwt-cli.rs
@@ -7,7 +7,7 @@ mod tests {
         encoding_key_from_secret, is_payload_item, is_timestamp_or_duration, translate_algorithm,
         OutputFormat, Payload, PayloadItem, SupportedAlgorithms,
     };
-    use base64::{decode as base64_decode};
+    use base64::decode as base64_decode;
     use chrono::{Duration, TimeZone, Utc};
     use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, TokenData};
     use serde_json::{from_value, json};
@@ -659,7 +659,8 @@ mod tests {
     #[test]
     fn encoding_key_from_secret_handles_at() {
         let expected = EncodingKey::from_secret(include_bytes!("hmac-key.bin"));
-        let key = encoding_key_from_secret(&Algorithm::HS256, "@./tests/hmac-key.bin", false).unwrap();
+        let key =
+            encoding_key_from_secret(&Algorithm::HS256, "@./tests/hmac-key.bin", false).unwrap();
         assert_eq!(expected, key);
     }
 
