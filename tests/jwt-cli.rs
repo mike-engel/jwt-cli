dadde35d411320c2653de525bd37d1a135d084eb
Carl Harris <ceharris@vt.edu>
2021-09-21 18:18:50 -0400

    use format macro to construct base64 args in test

--- tests/jwt-cli.rs
+++ tests/jwt-cli.rs
@@ -666,8 +666,7 @@ mod tests {
     #[test]
     fn encoding_key_from_secret_handles_base64() {
         let b64 = "+t0vs/PPB0dvyYKIk1DYvz5WyCUds5DLy07ycOK5oHA=";
-        let mut arg: String = "b64:".to_owned();
-        arg.push_str(b64);
+        let arg = format!("b64:{}", b64);
         let expected = EncodingKey::from_secret(&base64_decode(b64).unwrap());
         let key = encoding_key_from_secret(&Algorithm::HS256, &arg).unwrap();
         assert_eq!(expected, key);
@@ -683,8 +682,7 @@ mod tests {
     #[test]
     fn decoding_key_from_secret_handles_base64() {
         let b64 = "+t0vs/PPB0dvyYKIk1DYvz5WyCUds5DLy07ycOK5oHA=";
-        let mut arg: String = "b64:".to_owned();
-        arg.push_str(b64);
+        let arg = format!("b64:{}", b64);
         let expected = DecodingKey::from_secret(&base64_decode(b64).unwrap()).into_static();
         let key = decoding_key_from_secret(&Algorithm::HS256, &arg).unwrap();
         assert_eq!(expected, key);
